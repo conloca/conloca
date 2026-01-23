44d0993bb05aef5de6aab68e01a998191fdfeddf
Niko Tsiklauri <n.tsiklauri02@gmail.com>
2026-01-23 20:19:44 +0400

    chore(devenv): add git to devenv packages

--- tooling/direnv/devenv.nix
+++ tooling/direnv/devenv.nix
@@ -21,6 +21,7 @@ in {
     # Bun.sh for javascript dependencies
     bun
     # Git hooks and formatters
+    git
     git-format-staged
     jq # Used in pre-commit hook and generally useful
     alejandra # Nix formatter
